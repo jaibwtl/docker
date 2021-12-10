86a862e0163faa0843cca61233511b9ba1fa5bbd
Jaibw <jai.unix@gmail.com>
2021-12-10 15:22:36 +0530

    Create install-jenkins-ubuntu20.sh

---
+++ install-jenkins-ubuntu20.sh
@@ -0,0 +1,8 @@
+apt install -y openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
